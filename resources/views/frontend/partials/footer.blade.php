28b26383a8b972c87f1fe3b4f76ddf7312a958fb
Satria Putra Ramadhan <satriaaprr99@gmail.com>
2021-11-18 14:20:04 +0700

    push cik

--- resources/views/frontend/partials/footer.blade.php
+++ resources/views/frontend/partials/footer.blade.php
@@ -42,19 +42,19 @@
            <div class="ftco-footer-widget mb-4">
             <h2 class="ftco-heading-2">Instagram</h2>
             <div class="thumb d-sm-flex">
-                  <a href="#" class="thumb-menu img" style="background-image: url(images/insta-1.jpg);">
+                  <a href="#" class="thumb-menu img" style="background-image: url(template/frontend/images/insta-1.jpg);">
                   </a>
-                  <a href="#" class="thumb-menu img" style="background-image: url(images/insta-2.jpg);">
+                  <a href="#" class="thumb-menu img" style="background-image: url(template/frontend/images/insta-2.jpg);">
                   </a>
-                  <a href="#" class="thumb-menu img" style="background-image: url(images/insta-3.jpg);">
+                  <a href="#" class="thumb-menu img" style="background-image: url(template/frontend/images/insta-3.jpg);">
                   </a>
               </div>
               <div class="thumb d-flex">
-                  <a href="#" class="thumb-menu img" style="background-image: url(images/insta-4.jpg);">
+                  <a href="#" class="thumb-menu img" style="background-image: url(template/frontend/images/insta-4.jpg);">
                   </a>
-                  <a href="#" class="thumb-menu img" style="background-image: url(images/insta-5.jpg);">
+                  <a href="#" class="thumb-menu img" style="background-image: url(template/frontend/images/insta-5.jpg);">
                   </a>
-                  <a href="#" class="thumb-menu img" style="background-image: url(images/insta-6.jpg);">
+                  <a href="#" class="thumb-menu img" style="background-image: url(template/frontend/images/insta-6.jpg);">
                   </a>
               </div>
           </div>
