96d09f0c76483eb3fe34a4efdd1a4939a528cbb9
Satria Putra Ramadhan <satriaaprr99@gmail.com>
2021-11-19 16:20:06 +0700

    fix

--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -76,26 +76,27 @@
     @yield('content')
     @include('frontend.partials.footer')
 </body>
+@include('frontend.layouts.js')
 <script src="{{asset('assets/frontend/js/libs/require.js')}}" data-main="{{asset('assets/frontend/js/index.js')}}"></script> 
 <script>
 function confirmation() {
   Swal.fire({
-  title: '<strong>HTML <u>example</u></strong>',
-  icon: 'info',
-  html:
-    'You can use <b>bold text</b>, ' +
-    '<a href="//sweetalert2.github.io">links</a> ' +
-    'and other HTML tags',
-  showCloseButton: true,
-  showCancelButton: true,
-  focusConfirm: false,
-  confirmButtonText:
-    '<i class="fa fa-thumbs-up"></i> Great!',
-  confirmButtonAriaLabel: 'Thumbs up, great!',
-  cancelButtonText:
-    '<i class="fa fa-thumbs-down"></i>',
-  cancelButtonAriaLabel: 'Thumbs down'
-})
-});
+    title: '<strong>HTML <u>example</u></strong>',
+    icon: 'info',
+    html:
+      'You can use <b>bold text</b>, ' +
+      '<a href="//sweetalert2.github.io">links</a> ' +
+      'and other HTML tags',
+    showCloseButton: true,
+    showCancelButton: true,
+    focusConfirm: false,
+    confirmButtonText:
+      '<i class="fa fa-thumbs-up"></i> Great!',
+    confirmButtonAriaLabel: 'Thumbs up, great!',
+    cancelButtonText:
+      '<i class="fa fa-thumbs-down"></i>',
+    cancelButtonAriaLabel: 'Thumbs down'
+  })
+};
 </script>
 </html>
--- resources/views/frontend/partials/footer.blade.php
+++ resources/views/frontend/partials/footer.blade.php
@@ -1,19 +1,18 @@
 <footer class="ftco-footer ftco-bg-dark ftco-section">
     <div class="container">
-      <div class="row mb-5">
+      <div class="row">
         <div class="col-md-6 col-lg-3">
-          <div class="ftco-footer-widget mb-4">
+          <div class="ftco-footer-widget">
             <h2 class="ftco-heading-2">Kusina</h2>
-            <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-            <ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-3">
+            <ul class="ftco-footer-social list-unstyled float-md-left float-lft">
               <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
               <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
               <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
             </ul>
           </div>
         </div>
-        <div class="col-md-6 col-lg-3">
-          <div class="ftco-footer-widget mb-4">
+        {{-- <div class="col-md-6">
+          <div class="ftco-footer-widget">
             <h2 class="ftco-heading-2">Open Hours</h2>
             <ul class="list-unstyled open-hours">
               <li class="d-flex"><span>Monday</span><span>9:00 - 24:00</span></li>
@@ -25,7 +24,7 @@
               <li class="d-flex"><span>Sunday</span><span> Closed</span></li>
             </ul>
           </div>
-        </div>
+        </div> --}}
         <div class="col-md-6 col-lg-3">
           <div class="ftco-footer-widget mb-4">
               <h2 class="ftco-heading-2">Newsletter</h2>
@@ -38,7 +37,7 @@
             </form>
           </div>
         </div>
-        <div class="col-md-6 col-lg-3">
+        {{-- <div class="col-md-6 col-lg-3">
            <div class="ftco-footer-widget mb-4">
             <h2 class="ftco-heading-2">Instagram</h2>
             <div class="thumb d-sm-flex">
@@ -58,7 +57,7 @@
                   </a>
               </div>
           </div>
-        </div>
+        </div> --}}
       </div>
       <div class="row">
         <div class="col-md-12 text-center">
